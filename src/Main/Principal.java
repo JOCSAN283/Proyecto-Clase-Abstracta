5ceeeebf430229da5243f3b2d0cc00d20576ad89
JOCSAN283 <jocsanramirez283@gmail.com.com>
2025-11-28 17:09:15 -0600

    Cambios en la nueva rama

--- src/Main/Principal.java
+++ src/Main/Principal.java
@@ -21,6 +21,7 @@ public class Principal {
             System.out.println("5--- Mostrar todos los profesionistas");
             System.out.println("6--- Salir");
             System.out.println("Hola mundo");
+            System.out.println("Cambios a la nueva rama");
 
             try {
                 opcion = lectura.nextInt();
